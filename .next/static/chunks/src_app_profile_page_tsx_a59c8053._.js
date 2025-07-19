07a1b3ebc71a801b4b7a211bfedf7b14837471d0
Henry Gadepe <48880500+henrygadepe@users.noreply.github.com>
2025-07-19 20:33:52 +0530

    Add .next/static/chunks/src_app_profile_page_tsx_a59c8053._.js 1752937431858

---
+++ .next/static/chunks/src_app_profile_page_tsx_a59c8053._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_47a0488e._.js"
+],
+    source: "dynamic"
+});
